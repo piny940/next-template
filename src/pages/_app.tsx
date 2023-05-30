d95b6088c33cd5806b741f51fb06e42ec64f24ad
piny940 <an.s.shoma@gmail.com>
2023-05-31 00:27:54 +0900

    bootstrapをインストール

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    // import('bootstrap')
+    import('bootstrap')
   }, [])
 
   return (
