62ffa3077a46927b5826397c7a66bd1351508533
piny940 <an.s.shoma@gmail.com>
2023-06-01 00:18:26 +0900

    Appのテストを作成

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,10 +1,11 @@
 import { useTheme } from '@/context/ThemeProvider'
+import { TestID } from '@/resources/TestID'
 
 export const App: React.FC = () => {
   const { theme } = useTheme()
 
   return (
-    <div data-bs-theme={theme} id="app">
+    <div data-bs-theme={theme} id="app" data-testid={TestID.APP}>
       Next template
     </div>
   )
