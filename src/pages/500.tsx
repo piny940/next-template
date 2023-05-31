9be8bcd0fc01de0be2a8805ba7ea622d807d21c5
piny940 <an.s.shoma@gmail.com>
2023-06-01 02:10:44 +0900

    500ページのテストを作成

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,8 +1,9 @@
+import { TestID } from '@/resources/TestID'
 import Link from 'next/link'
 
 const Custom500: React.FC = () => {
   return (
-    <div className="container">
+    <div className="container" data-testid={TestID.CUSTOM500}>
       <h1>サーバーでエラーが発生しました。</h1>
       <p>
         <Link href="/">ホームに戻る</Link>
--- src/resources/TestID.ts
+++ src/resources/TestID.ts
@@ -2,4 +2,5 @@ export enum TestID {
   TEST = 'test',
   APP = 'app',
   CUSTOM404 = 'custom404',
+  CUSTOM500 = 'custom500',
 }
