bc01fda961d5dadcdd71d7fda2ac146ea502c1c9
piny940 <an.s.shoma@gmail.com>
2023-06-01 02:05:10 +0900

    テストを分割する

--- __tests__/contexts/ThemeProvider.test.tsx
+++ __tests__/contexts/ThemeProvider.test.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from 'react'
 import { renderHook, waitFor, act } from '@testing-library/react'
 
 describe('<ThemeProvider />', () => {
-  it('正常にテーマを取得・更新できる', async () => {
+  it('正常にテーマを取得できる', async () => {
     const wrapper = ({ children }: { children: ReactNode }) => {
       return <ThemeProvider>{children}</ThemeProvider>
     }
@@ -13,6 +13,12 @@ describe('<ThemeProvider />', () => {
     await waitFor(() => {
       expect(result.current.theme).toBe('light')
     })
+  })
+  it('正常にテーマを更新できる', async () => {
+    const wrapper = ({ children }: { children: ReactNode }) => {
+      return <ThemeProvider>{children}</ThemeProvider>
+    }
+    const { result } = renderHook(() => useTheme(), { wrapper: wrapper })
 
     act(() => {
       result.current.setTheme('dark')
