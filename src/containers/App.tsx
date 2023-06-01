a6eb16c861feb9e19a63d2fe99c3f927511fe427
piny940 <an.s.shoma@gmail.com>
2023-06-01 10:00:15 +0900

    メインカラーを使えるようにする

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,11 +1,8 @@
-import { useTheme } from '@/context/ThemeProvider'
 import { TestID } from '@/resources/TestID'
 
 export const App: React.FC = () => {
-  const { theme } = useTheme()
-
   return (
-    <div data-bs-theme={theme} id="app" data-testid={TestID.APP}>
+    <div id="app" data-testid={TestID.APP}>
       Next template
     </div>
   )
--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -5,11 +5,11 @@ export const Navbar: React.FC = () => {
   return (
     <nav
       data-testid={TestID.NAVBAR}
-      className="navbar navbar-expand navbar-dark bg-dark"
+      className="navbar navbar-expand navbar-dark bg-main"
     >
       <div className="container-fluid px-5">
         <Link href="/">
-          <h1 className="navbar-brand">Next Template</h1>
+          <div className="navbar-brand title fw-bold">Next Template</div>
         </Link>
       </div>
     </nav>
