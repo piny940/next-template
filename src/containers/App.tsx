76dc85abd4e70706ba7e3395f5a5d2ef78d72e10
Mikan <83708535+piny940@users.noreply.github.com>
2023-06-01 10:04:03 +0900

    メインカラーを使えるようにする (#12)
    * メインカラーを使えるようにする
    
    * 見た目を整える

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,12 +1,10 @@
-import { useTheme } from '@/context/ThemeProvider'
 import { TestID } from '@/resources/TestID'
 
 export const App: React.FC = () => {
-  const { theme } = useTheme()
-
   return (
-    <div data-bs-theme={theme} id="app" data-testid={TestID.APP}>
-      Next template
+    <div id="app" data-testid={TestID.APP}>
+      <h1>Next template</h1>
+      <p>This is a template repository of next project.</p>
     </div>
   )
 }
