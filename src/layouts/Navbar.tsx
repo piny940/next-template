9feac41de7a40c6e96b22982f8d5f173e0eae0f0
piny940 <an.s.shoma@gmail.com>
2023-06-28 21:34:26 +0900

    navbarの見た目を修正

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,16 +1,25 @@
 import { IconButton } from '@/components/Navbar/IconButton'
+import { useTheme } from '@/context/ThemeProvider'
 import { TestID } from '@/resources/TestID'
 import Link from 'next/link'
 
 export const Navbar: React.FC = () => {
+  const { theme } = useTheme()
+
   return (
     <nav
       data-testid={TestID.NAVBAR}
-      className="navbar navbar-expand navbar-light bg-light"
+      className={
+        'navbar navbar-expand-lg ' +
+        (theme === 'light' ? 'navbar-light bg-light ' : 'navbar-dark bg-dark')
+      }
     >
       <div className="container-fluid px-5">
-        <Link href="/">
-          <div className="navbar-brand title fw-bold">Next Template</div>
+        <Link
+          href="/"
+          className="title fw-bold d-flex align-items-center text-body"
+        >
+          <div>Next Template</div>
         </Link>
         <IconButton theme="dark" onClick={() => undefined} />
       </div>
